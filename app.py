a0bd18eb019ba4f2a5ea336058f5a0a33f37a4df
Darren Bono <dbono215@gmail.com>
2023-10-15 16:18:44 -0400

    Add first path selection

--- app.py
+++ app.py
@@ -1,111 +1,132 @@
 # -*- mode: python; python-indent: 4 -*-
 """Docstring missing."""
-import re
-
 import streamlit as st
 
 from mage import Mage
 from openaichat import OpenAi
+from toolbox import Toolbox
 
 _CHARACTERS = ["", "Warrior", "Hunter", "Mage"]
 
 
-class Application(OpenAi):
+class Application(OpenAi, Toolbox):
     """Docstring missing."""
 
     def __init__(self):
         """Docstring missing."""
         OpenAi.__init__(self)
-
-        if 'start' not in st.session_state:
-            st.session_state.start = False
+        Toolbox.__init__(self)
         
         if "messages" not in st.session_state.keys():
             self.initialize_chat()
     
     def initialize_chat(self) -> None:
-        st.session_state.messages = [
-            {"role": "assistant", "content": "Select a character on the left to begin"}
-        ]
-        st.session_state.start = False
-    
-    def get_selection(self, number, response):
-        """Docstring missing."""
-        pattern = rf"{number}\.\s(.*?):"
-        match = re.search(pattern, response)
-        
-        return match.group(1) if re.search(pattern, response) else None
+        st.session_state.messages = []
+        st.session_state.start = {"active": False, "disabled": False}
+        st.session_state.character = {"active": False, "disabled": False}
+        st.session_state.first_path = {"active": False, "disabled": False}
+        st.session_state.second_path = {"active": False, "disabled": False}
+        st.session_state.character_selection = None
     
     def click_button(self, name):
-        st.session_state[name] = True
+        st.session_state[name]["active"] = True
+        st.session_state[name]["disabled"] = True
     
     def main(self):
         """Docstring missing."""
         st.set_page_config(layout="wide", page_title="Streamlit Adventure Game")
         st.header("Streamlit Adventure Game")
-        character_selection = st.sidebar.selectbox("Select a character", _CHARACTERS, key="character")
+        st.markdown(
+            "###### _Welcom to the my Streamlit Adventure Game! To begin, select a charcter on \
+            the left and press 'Start!'_"
+        )
+        st.sidebar.selectbox(
+            "Select a character",
+            _CHARACTERS,
+            on_change=self.click_button,
+            args=('character',),
+            disabled=st.session_state["character"]["disabled"],
+            key="character_selection"
+        )
 
-        if character_selection == "Mage":
-            character = Mage()
+        match st.session_state.character_selection:
+            case "Mage":
+                character = Mage()
 
-        if character_selection:
-            st.sidebar.write(f"**Character:** {character_selection}")
+        if st.session_state.character_selection:
+            st.sidebar.write(f"**Character:** {st.session_state.character_selection}")
             st.sidebar.write(f"**Weapon:** {character.weapon}")
-            st.sidebar.button("Start!", on_click=self.click_button, args=('start',))
+            st.sidebar.button(
+                "Start!",
+                on_click=self.click_button,
+                args=('start',),
+                disabled=st.session_state["start"]["disabled"],
+                use_container_width=True
+            )
 
-        # if "messages" not in st.session_state.keys():
-        #     st.session_state.messages = [
-        #         {
-        #             "role": "assistant",
-        #             "content": "Select a character on the left to begin"
-        #         }
-        #     ]
-        
         for message in st.session_state.messages:
             with st.chat_message(message["role"]):
                 st.write(message["content"])
-
-        # if st.session_state.start:
-        #     with st.chat_message("assistant"):
-        #         with st.spinner("Generating Adventure..."):
-        #             response = self.generate_response(
-        #                 f"""
-        #                 In no more than 250 words, create an introduction to a story in 
-        #                 Medieval times about a {character_selection} with a magical 
-        #                 {character.weapon} on a quest. Provide 4 choices as numbered 
-        #                 selections, each with a title, for the {character_selection} 
-        #                 to choose from.
-        #                 """
-        #             )
-        #             st.write(response)
-
-        #     message = {"role": "assistant", "content": response}
-        #     st.session_state.messages.append(message)
-
-        #     with st.chat_message("assistant"):
-        #         st.write("Choose your path...")
-            
-        #     choices = []
-        #     for item in [1, 2, 3, 4]:
-        #         choices.append(self.get_selection(item, response))
-            
-        #     st.write(choices)
         
-        st.sidebar.button("Restart Game", on_click=self.initialize_chat, key="clear")
+        if st.session_state["start"]["active"]:
+            with st.spinner("Generating Adventure..."):
+                intro = self.generate_response(
+                    f"""
+                    In no more than 200 words, create an introduction to a story in 
+                    Medieval times about a {st.session_state.character_selection} with 
+                    a magical {character.weapon} on a quest. Provide 4 choices as 
+                    numbered selections, each with a title, for the 
+                    {st.session_state.character_selection} to choose from.
+                    """
+                )
 
+                st.session_state.messages.append(
+                    {"role": "assistant", "content": intro}
+                )
 
-        # if prompt := st.chat_input(disabled=not(character_selection)):
-        #     st.session_state.messages.append({"role": "user", "content": prompt})
-        #     with st.chat_message("user"):
-        #         st.write(prompt)
-        
-        # if st.session_state.messages[-1]["role"] != "assistant":
-        #     with st.chat_message("assistant"):
-        #         with st.spinner("Thinking..."):
-        #             response = self.generate_response(prompt) 
-        #             st.write(response) 
-        #     message = {"role": "assistant", "content": response}
-        #     st.session_state.messages.append(message)
+            with st.chat_message("assistant"):
+                st.markdown(intro)
+            
+            st.session_state["start"]["active"] = False
+            st.session_state["first_path"]["active"] = True
+            st.session_state.first_path_choices = [
+                self.get_selection(item, intro) for item in range(1,5)
+            ]
+
+        if st.session_state["first_path"]["active"]:
+            if first_path_selection := st.radio(
+                "Choose your path...",
+                st.session_state.first_path_choices,
+                label_visibility="visible",
+                horizontal=True,
+                index=None,
+                on_change=self.click_button,
+                args=('first_path',),
+                disabled=st.session_state["first_path"]["disabled"],
+                key="first_path_selection"
+            ):
+
+                with st.spinner(f"Contuining on to the {first_path_selection}..."):
+                    path = self.generate_response(
+                        f"""
+                        In no more than 150 words, continue the story with the
+                        {st.session_state.character_selection} selecting the 
+                        {first_path_selection} path. Provide 4 additional choices as 
+                        numbered selections, each with a title to choose from.
+                        """
+                    )
+
+                with st.chat_message("assistant"):
+                    st.markdown(path)
+                
+                st.session_state["second_path"]["active"] = True
+
+        if st.session_state["character"]["active"]:
+            st.sidebar.button(
+                "Reset Game",
+                on_click=self.initialize_chat,
+                use_container_width=True
+            )
 
 if __name__ == '__main__':
     Application().main()
--- toolbox.py
+++ toolbox.py
@@ -1,5 +1,6 @@
 # -*- mode: python; python-indent: 4 -*-
 """Docstring Missing."""
+import re
 
 from loggerfactory import LoggerFactory
 
@@ -10,3 +11,10 @@ class Toolbox():
     def __init__(self):
         """Docstring missing."""
         self.logger = LoggerFactory.get_logger("app.py", log_level="WARNING")
+    
+    def get_selection(self, number, response):
+        """Docstring missing."""
+        pattern = rf"{number}\.\s(.*?):"
+        match = re.search(pattern, response)
+        
+        return match.group(1) if re.search(pattern, response) else None
